436fdc3ee2b4a11623f64c1f596f2b7a28054cb6
thomasrobinsongore <trg103@gmail.com>
2014-08-18 00:45:29 +0100

    Added another string

--- src/com/thomas/Main.java
+++ src/com/thomas/Main.java
@@ -7,6 +7,8 @@ public class Main {
         ToStringExample stringExample = new ToStringExample(30, 11, 86);
         System.out.printf("My birthday is %s", stringExample);              //passing an object into a string
 
+        System.out.println("This is another string");
+
     }
 
 }
